14d5f18ae9bcb50201a31a527380d13e5e385721
Sebastiaan <sebas.ram13@gmail.com>
2023-04-30 23:04:16 +0200

    Working Vue environment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8000,
+    watch: {
+      usePolling: true,
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
