514232d60452272f2e391c83daedf56810f0e007
Stijn Groenen <hello@stijngroenen.nl>
2023-07-13 15:57:55 +0200

    Added multiple results view

--- src/components/ResultsTab.vue
+++ src/components/ResultsTab.vue
@@ -1,30 +1,43 @@
 <template>
-    <v-container fluid fill-height>
-        <v-row id="results">
-            <v-col col="1"></v-col>
-            <v-col cols="3">
-                <SpiderChartInfo />
-            </v-col>
-            <v-col cols="7" class="results-item">
-                <SpiderChart />
-            </v-col>
-            <v-col cols="1"></v-col>
-        </v-row>
-    </v-container>
+    <template v-if="getSelectedEssays().length >= 1">
+        <div v-for="essay in getSelectedEssays()" class="timeline-container">
+            <h3>{{ essay.name }}</h3>
+            <v-container fluid fill-height>
+                <v-row id="results">
+                    <v-col col="1"></v-col>
+                    <v-col cols="3">
+                        <LearningGainInfo :pplg="essay.pplg" />
+                    </v-col>
+                    <v-col cols="7" class="results-item">
+                        <SpiderChart :pplg="essay.pplg" :spider-data="essay.spiderData" />
+                    </v-col>
+                    <v-col cols="1"></v-col>
+                </v-row>
+            </v-container>
+        </div>
+    </template>
+    <template v-else>
+        <h1>Er zijn geen betogen geselecteerd.</h1>
+    </template>
 </template>
 
 <script>
 import SpiderChart from "./SpiderChart.vue";
-import SpiderChartInfo from "./info/SpiderChartInfo.vue";
+import LearningGainInfo from "./info/LearningGainInfo.vue";
 
 export default {
     name: "ResultsTab",
-    components: {SpiderChartInfo, SpiderChart}
+    components: { LearningGainInfo, SpiderChart },
+    methods: {
+        getSelectedEssays() {
+            return this.$store.getters.selectedEssays;
+        }
+    }
 }
 </script>
 
 <style>
-#results .results-item{
+#results .results-item {
     background-color: #d5f1f8;
     border-radius: 20px;
     margin: 20px 0;
--- src/components/info/LearningGainInfo.vue
+++ src/components/info/LearningGainInfo.vue
@@ -1,40 +1,39 @@
 <template>
-    <v-row no-gutters>
-        <v-col cols="6">
-            <span>Voor meting</span>
-            <br>
-            <span class="tab-info-title">{{ pplg[0] }}</span>
-            <br><br>
-            <span>Na meting</span>
-            <br>
-            <span class="tab-info-title">{{ pplg[1] }}</span>
-        </v-col>
-        <v-col cols="6" id="learning-gain-profit-col">
-            <span class="learning-gain-profit-text">Leerwinst</span>
-            <span class="tab-info-title tab-info-big-title learning-gain-profit-text">{{ pplg[2] }}</span>
-        </v-col>
+    <v-row class="results-item">
+        <v-row no-gutters>
+            <v-col cols="6">
+                <span>Voor meting</span>
+                <br>
+                <span class="tab-info-title">{{ pplg[0] }}</span>
+                <br><br>
+                <span>Na meting</span>
+                <br>
+                <span class="tab-info-title">{{ pplg[1] }}</span>
+            </v-col>
+            <v-col cols="6" id="learning-gain-profit-col">
+                <span class="learning-gain-profit-text">Leerwinst</span>
+                <span class="tab-info-title tab-info-big-title learning-gain-profit-text">{{ pplg[2] }}</span>
+            </v-col>
+        </v-row>
     </v-row>
 </template>
 
 <script>
 export default {
     name: "LearningGainInfo",
-    data: function() {
-        return {
-            pplg: this.$store.getters.essays[0].pplg,
-        };
-    },
+    props: ['pplg'],
 }
 </script>
 
 <style scoped>
-#learning-gain-profit-col{
-    display:flex;
+#learning-gain-profit-col {
+    display: flex;
     text-align: center;
     align-items: center;
     flex-wrap: inherit;
 }
-.learning-gain-profit-text{
+
+.learning-gain-profit-text {
     display: block;
     width: 100%;
 }
